9c9caff85e9f4642789b4417816c30e96bf3fd96
Levi Thomason <me@levithomason.com>
2016-08-24 18:46:13 -0700

    fix(Select): use defaultProps

--- src/addons/Select/Select.js
+++ src/addons/Select/Select.js
@@ -4,14 +4,15 @@ import { getElementType, getUnhandledProps, META } from '../../lib'
 import { Dropdown } from '../../modules'
 
 /**
- * A <Select /> is sugar for <Dropdown selection />.
+ * A Select is sugar for <Dropdown selection />.
  * @see Dropdown
  * @see Form
  */
 function Select(props) {
+  const { selection } = props
   const ElementType = getElementType(Select, props)
   const rest = getUnhandledProps(Select, props)
-  return <ElementType {...rest} selection />
+  return <ElementType {...rest} selection={selection} />
 }
 
 Select._meta = {
@@ -20,15 +21,13 @@ Select._meta = {
 }
 
 Select.propTypes = {
-  /** An element type to render as (string or function). */
-  as: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]),
+  /** selection value */
+  selection: PropTypes.bool,
 }
 
 Select.defaultProps = {
   as: Dropdown,
+  selection: true,
 }
 
 export default Select
